90608c4b664b0f1ea8ddb43d24a2fd24631384d2
Muh. Ikhsan Amir <ikhsn.ar@gmail.com>
2021-07-14 15:17:20 +0700

    UI Rev1

---
+++ Maimon/ViewController/addExpenseViewController.swift
@@ -0,0 +1,33 @@
+//
+//  addExpenseViewController.swift
+//  Maimon
+//
+//  Created by San on 14/07/21.
+//
+
+import UIKit
+
+class addExpenseViewController: UIViewController {
+
+    
+    @IBOutlet weak var itemExpense: UITextField!
+    @IBOutlet weak var categoryExpense: UITextField!
+    @IBOutlet weak var priceExpense: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Maimon/ViewController/addIncomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  addIncomeViewController.swift
+//  Maimon
+//
+//  Created by San on 14/07/21.
+//
+
+import UIKit
+
+class addIncomeViewController: UIViewController {
+
+    @IBOutlet weak var priceIncome: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
