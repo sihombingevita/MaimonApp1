d03014355e33d0863d07c351208bb888ec95bfe6
Evita Sihombing <veronikaevita@gmail.com>
2021-07-14 10:50:43 +0700

    Projek MC-3 Maimon v1

---
+++ Maimon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maimon
+//
+//  Created by Evita Sihombing on 14/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
