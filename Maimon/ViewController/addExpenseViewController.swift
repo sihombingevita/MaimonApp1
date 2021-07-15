60a32a0ec5a864ee89e7fb3f4de7418fe31c9bfc
Muh. Ikhsan Amir <ikhsn.ar@gmail.com>
2021-07-15 13:45:07 +0700

    datePIcker Screen addIncome Rev1

--- Maimon/ViewController/addExpenseViewController.swift
+++ Maimon/ViewController/addExpenseViewController.swift
@@ -13,21 +13,17 @@ class addExpenseViewController: UIViewController {
     @IBOutlet weak var itemExpense: UITextField!
     @IBOutlet weak var categoryExpense: UITextField!
     @IBOutlet weak var priceExpense: UITextField!
+    @IBOutlet weak var dateExpense: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        itemExpense.layer.cornerRadius = 15
+        itemExpense.layer.borderWidth = 0.5
+        categoryExpense.layer.cornerRadius = 15
+        categoryExpense.layer.borderWidth = 0.5
+        priceExpense.layer.cornerRadius = 15
+        priceExpense.layer.borderWidth = 0.5
+        dateExpense.layer.cornerRadius = 15
+        dateExpense.layer.borderWidth = 0.5
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Maimon/ViewController/addIncomeViewController.swift
+++ Maimon/ViewController/addIncomeViewController.swift
@@ -13,83 +13,51 @@ class addIncomeViewController: UIViewController {
     @IBOutlet weak var dateIncome: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        priceIncome.keyboardType = .numberPad
+        //priceIncome.keyboardType = .numberPad
         priceIncome.layer.cornerRadius = 15
         priceIncome.layer.borderWidth = 0.5
         
         dateIncome.layer.cornerRadius = 15
         dateIncome.layer.borderWidth = 0.5
         
-        self.dateIncome.datePicker(target: self, doneAction: #selector(doneAction), cancelAction: #selector(cancelAction), datePickerMode: .date)
-        
-
-        // Do any additional setup after loading the view.
-    }
-    
-    @objc
-    func cancelAction() {
-        self.dateIncome.resignFirstResponder()
-    }
-
-    @objc
-    func doneAction() {
-        if let datePickerView = self.dateIncome.inputView as? UIDatePicker {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "dd-MM-yyyy"
-            let dateString = dateFormatter.string(from: datePickerView.date)
-            self.dateIncome.text = dateString
+        self.dateIncome.setInputViewDatePicker(target: self, selector: #selector(tapDone)) //1
+            }
             
-            print(datePickerView.date)
-            print(dateString)
-            
-            self.dateIncome.resignFirstResponder()
-        }
-    }
-
+            //2
+            @objc func tapDone() {
+                if let datePicker = self.dateIncome.inputView as? UIDatePicker { // 2-1
+                    let dateformatter = DateFormatter() // 2-2
+                    dateformatter.dateStyle = .medium // 2-3
+                    self.dateIncome.text = dateformatter.string(from: datePicker.date) //2-4
+                }
+                self.dateIncome.resignFirstResponder() // 2-5
+            }
 }
 extension UITextField {
-    func datePicker<T>(target: T,
-                       doneAction: Selector,
-                       cancelAction: Selector,
-                       datePickerMode: UIDatePicker.Mode = .date) {
+    
+    func setInputViewDatePicker(target: Any, selector: Selector) {
+        // Create a UIDatePicker object and assign to inputView
         let screenWidth = UIScreen.main.bounds.width
-        
-        func buttonItem(withSystemItemStyle style: UIBarButtonItem.SystemItem) -> UIBarButtonItem {
-            let buttonTarget = style == .flexibleSpace ? nil : target
-            let action: Selector? = {
-                switch style {
-                case .cancel:
-                    return cancelAction
-                case .done:
-                    return doneAction
-                default:
-                    return nil
-                }
-            }()
-            
-            let barButtonItem = UIBarButtonItem(barButtonSystemItem: style,
-                                                target: buttonTarget,
-                                                action: action)
-            
-            return barButtonItem
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))//1
+        datePicker.datePickerMode = .date //2
+        // iOS 14 and above
+        if #available(iOS 14, *) {// Added condition for iOS 14
+          datePicker.preferredDatePickerStyle = .wheels
+          datePicker.sizeToFit()
         }
+        self.inputView = datePicker //3
         
-        let datePicker = UIDatePicker(frame: CGRect(x: 0,
-                                                    y: 0,
-                                                    width: screenWidth,
-                                                    height: 216))
-        datePicker.datePickerMode = datePickerMode
-        self.inputView = datePicker
-        
-        let toolBar = UIToolbar(frame: CGRect(x: 0,
-                                              y: 0,
-                                              width: screenWidth,
-                                              height: 44))
-        toolBar.setItems([buttonItem(withSystemItemStyle: .cancel),
-                          buttonItem(withSystemItemStyle: .flexibleSpace),
-                          buttonItem(withSystemItemStyle: .done)],
-                         animated: true)
-        self.inputAccessoryView = toolBar
+        // Create a toolbar and assign it to inputAccessoryView
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0)) //4
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) //5
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: #selector(tapCancel)) // 6
+        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector) //7
+        toolBar.setItems([cancel, flexible, barButton], animated: false) //8
+        self.inputAccessoryView = toolBar //9
     }
+    
+    @objc func tapCancel() {
+        self.resignFirstResponder()
+    }
+    
 }
-
